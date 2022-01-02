11b790e32610a56652bbbe2e8064b5d8c88a2414
José Paulo Leal <zp@dcc.fc.up.pt>
2022-01-02 19:06:40 +0000

    XML parsing

--- test/xmlinjson.js
+++ test/xmlinjson.js
@@ -1,81 +1,158 @@
 const assert = require('assert');
-const { serialize }  = require('../xmlinjson.js');
+const { serialize, parse } = require('../xmlinjson.js');
 
-describe('Just the root element', () => {
+describe('Serializer', () => {
 
-   check({ "element": "top"},"<top/>");
-   check({ "element": "top", "attributes": {} },"<top/>");
-   check({ "element": "top", "attributes": {}, "content": [] },"<top/>");
-   check({ "element": "top", "content": [], "attributes": {} },"<top/>");
-   check({ "content": [], "attributes": {}, "element": "top" },"<top/>");
-});
+    describe('Just the root element', () => {
 
-describe('Invalid XIJ should thorw errors', () =>{
-    checkThrows({},/Invalid node type/);
-    checkThrows({ "ELEMENT": "top"},/Invalid node type/);
-    checkThrows({ "element": "top", "attributes": 1 },/Attributes must be an object/);
-    checkThrows({ "element": "top", "content": {} },/Content must be an array/);
-});
+        check({ "element": "top" }, "<top/>");
+        check({ "element": "top", "attributes": {} }, "<top/>");
+        check({ "element": "top", "attributes": {}, "content": [] }, "<top/>");
+        check({ "element": "top", "content": [], "attributes": {} }, "<top/>");
+        check({ "content": [], "attributes": {}, "element": "top" }, "<top/>");
+    });
 
-describe('The root element with attribues', () => {
-   check({ "element": "top", "attributes": { "a": 1}},'<top a="1"/>');
-   check({ "element": "top", "attributes": { "a": "b"}},'<top a="b"/>');
-   check({ "element": "top", "attributes": { "a": true}},'<top a="true"/>');
-   check({ "element": "top", "attributes": { "a": "1\""}},"<top a='1\"'/>");
-   check({ "element": "top", "attributes": { "a": 1, "b": 2}},'<top a="1" b="2"/>');
-   check({ "element": "top", "attributes": { "a": "hello world"}},'<top a="hello world"/>');
-});
+    describe('Invalid XIJ should throw errors', () => {
+        checkThrows({}, /Invalid node type/);
+        checkThrows({ "ELEMENT": "top" }, /Invalid node type/);
+        checkThrows({ "element": "top", "attributes": 1 }, /Attributes must be an object/);
+        checkThrows({ "element": "top", "content": {} }, /Content must be an array/);
+    });
 
-describe('The root element with text and attribues', () => {
-   check({ "element": "top","content": [ 1 ] },'<top>1</top>');
-   check({ "element": "top","content": [ true ] },'<top>true</top>');
-   check({ "element": "top","content": [ "hello world"]},'<top>hello world</top>');
-   check({ "element": "top","content": [ "hello world"]},'<top>hello world</top>');
-   check({ "element": "top", "attributes": { "a": 1}, "content": [ "hello world"]},'<top a="1">hello world</top>');
-   check({ "element": "top", "attributes": { "a": 1, "b": 2}, "content": [ "hello world"]},'<top a="1" b="2">hello world</top>');
-});
+    describe('The root element with attribues', () => {
+        check({ "element": "top", "attributes": { "a": 1 } }, '<top a="1"/>');
+        check({ "element": "top", "attributes": { "a": "b" } }, '<top a="b"/>');
+        check({ "element": "top", "attributes": { "a": true } }, '<top a="true"/>');
+        check({ "element": "top", "attributes": { "a": "1\"" } }, "<top a='1\"'/>");
+        check({ "element": "top", "attributes": { "a": 1, "b": 2 } }, '<top a="1" b="2"/>');
+        check({ "element": "top", "attributes": { "a": "hello world" } }, '<top a="hello world"/>');
+    });
+
+    describe('The root element with text and attribues', () => {
+        check({ "element": "top", "content": [1] }, '<top>1</top>');
+        check({ "element": "top", "content": [true] }, '<top>true</top>');
+        check({ "element": "top", "content": ["hello world"] }, '<top>hello world</top>');
+        check({ "element": "top", "content": ["hello world"] }, '<top>hello world</top>');
+        check({ "element": "top", "attributes": { "a": 1 }, "content": ["hello world"] }, '<top a="1">hello world</top>');
+        check({ "element": "top", "attributes": { "a": 1, "b": 2 }, "content": ["hello world"] }, '<top a="1" b="2">hello world</top>');
+    });
 
 
-describe('Data oriented (no mixed content)', () => {
+    describe('Data oriented (no mixed content)', () => {
 
-    check({ "element": "top","content":[
-        { "element": "a", "content": [ 1 ]},
-        { "element": "b", "content": [ 2 ]}
-    ]},"<top><a>1</a><b>2</b></top>");
-    check({ "element": "top","content":[
-        { "element": "middle", "content": [ { "element": "leaf" }, { "element": "leaf" } ]},
-        { "element": "middle", "content": [ { "element": "leaf" }, { "element": "leaf" } ]}
-    ]},"<top><middle><leaf/><leaf/></middle><middle><leaf/><leaf/></middle></top>");
-    check({ "element": "top","content":[
-        { "element": "middle", "content": [ { "element": "leaf", "content": [ 1 ] }, { "element": "leaf", "content": [ 2 ] } ]},
-        { "element": "middle", "content": [ { "element": "leaf", "content": [ 3 ] }, { "element": "leaf", "content": [ 4 ] } ]}
-    ]},"<top><middle><leaf>1</leaf><leaf>2</leaf></middle><middle><leaf>3</leaf><leaf>4</leaf></middle></top>");
-    
- });
+        check({
+            "element": "top", "content": [
+                { "element": "a", "content": [1] },
+                { "element": "b", "content": [2] }
+            ]
+        }, "<top><a>1</a><b>2</b></top>");
+        check({
+            "element": "top", "content": [
+                { "element": "middle", "content": [{ "element": "leaf" }, { "element": "leaf" }] },
+                { "element": "middle", "content": [{ "element": "leaf" }, { "element": "leaf" }] }
+            ]
+        }, "<top><middle><leaf/><leaf/></middle><middle><leaf/><leaf/></middle></top>");
+        check({
+            "element": "top", "content": [
+                { "element": "middle", "content": [{ "element": "leaf", "content": [1] }, { "element": "leaf", "content": [2] }] },
+                { "element": "middle", "content": [{ "element": "leaf", "content": [3] }, { "element": "leaf", "content": [4] }] }
+            ]
+        }, "<top><middle><leaf>1</leaf><leaf>2</leaf></middle><middle><leaf>3</leaf><leaf>4</leaf></middle></top>");
 
-describe('Mixed content', () => {
-    check({ "element": "top","content": [ 
-        "hello ",
-        { "element": "b","content": [ "new"] },
-        " world" ] },
-        "<top>hello <b>new</b> world</top>");
-        check({ "element": "top","content": [ 
-            "goodbye ",
-            { "element": "i","content": [ "old"] },
-            " and ",
-            { "element": "b","content": [ "funny"] },
-            " world" ] },
+    });
+
+    describe('Mixed content', () => {
+        check({
+            "element": "top", "content": [
+                "hello ",
+                { "element": "b", "content": ["new"] },
+                " world"]
+        },
+            "<top>hello <b>new</b> world</top>");
+        check({
+            "element": "top", "content": [
+                "goodbye ",
+                { "element": "i", "content": ["old"] },
+                " and ",
+                { "element": "b", "content": ["funny"] },
+                " world"]
+        },
             '<top>goodbye <i>old</i> and <b>funny</b> world</top>');
+    });
+
+    describe('Comment', () => {
+        check({ "element": "top", "content": [{ "comment": "this is a comment" }] },
+            '<top><!--this is a comment--></top>');
+    });
+
+    describe('Processing instruction', () => {
+        check({ "element": "top", "content": [{ "processingInstruction": "xij", "attributes": { "indent": true } }] },
+            '<top><?xml-xij indent="true"?></top>');
+    });
+
 });
 
-describe('Comment', () => {
-    check({ "element": "top","content": [ { "comment": "this is a comment" } ]},
-    '<top><!--this is a comment--></top>');
-});
+describe('Parser', () => {
 
-describe('Processing instruction', () => {
-    check({ "element": "top","content": [ { "processingInstruction": "xij", "attributes": { "indent": true } } ]},
-    '<top><?xml-xij indent="true"?></top>');
+    const examples = [
+        {
+            description: 'Just root element',
+            xml: '<top/>',
+            json: {element: 'top'}
+        },
+        {
+            description: 'Root element just with attributes',
+            xml: '<top a="1" b="2"/>',
+            json: {element: 'top', attributes: { a: 1, b: 2}}
+        },
+        {
+            description: 'Root element and attributes with different delimiters',
+            xml: '<top a="1" b=\'2"\' c="hello world"/>',
+            json: {element: 'top', attributes: { a: 1, b: '2"', c:"hello world"}}
+        },
+        {
+            description: 'With text content',
+            xml: '<top>hello world</top>',
+            json: {element: 'top', content: [ 'hello world' ]}
+        },
+        {
+            description: 'With attributes and text content',
+            xml: '<top a="hello" b="world">hello world</top>',
+            json: {element: 'top', attributes: { a: 'hello', b: 'world'}, content: [ 'hello world' ]}
+        },
+        {
+            description: 'With mixed  content',
+            xml: '<top>hello <b>my</b> world</top>',
+            json: {element: 'top', content: [ 'hello ', { element: 'b', content: [ 'my' ]}, ' world' ]}
+        },
+        {
+            description: 'With mixed  content and attributes',
+            xml: '<top a="1" b="2">hello <b>my</b> world</top>',
+            json: {element: 'top', attributes: { a: 1, b: 2}, content: [ 'hello ', { element: 'b', content: [ 'my' ]}, ' world' ]}
+        },
+        {
+            description: 'With a comment',
+            xml: '<top><!-- hello world --></top>',
+            json: {element: 'top', content: [ { comment: " hello world " } ]}
+        },
+
+
+    ]
+
+    for(const example of examples)
+        describe(example.description, () => {
+            const json = parse(example.xml);
+
+            it('parse', (done) => { 
+                assert.deepEqual(json,example.json); 
+                done(); 
+            });
+
+            it('parse and serialize', (done) => {
+                assert.equal(serialize(json),example.xml);
+                done();
+            });
+        });
 });
 
 /*----------------*\
@@ -83,13 +160,13 @@ describe('Processing instruction', () => {
  \*---------------*/
 
 
-function check(xij,xml) {
-    it(xml, (done ) => { assert.equal(serialize(xij),xml); done(); });
+function check(xij, xml) {
+    it(xml, (done) => { assert.equal(serialize(xij), xml); done(); });
 }
 
-function checkThrows(xij,pattern) {
+function checkThrows(xij, pattern) {
     it(`${JSON.stringify(xij)} should report ${pattern}`, (done) => {
-        assert.throws( () => serialize(xij), { name: "Error", message: pattern });
+        assert.throws(() => serialize(xij), { name: "Error", message: pattern });
         done();
     });
 }
--- xmlinjson.js
+++ xmlinjson.js
@@ -22,118 +22,340 @@
 \********************************************************************/
 
 const { Buffer } = require('buffer');
-
-function serialize(json) {
-
-    offset = 0;
-    makeNode(json);
-    return BUILDER.toString('utf-8',0,offset);
-}
+const { serialize } = require('v8');
 
 const BUFFER_SIZE = 1024;
-const BUILDER = Buffer.alloc(BUFFER_SIZE);
-let offset = 0;
 
-function write(value) {
-    const text   = typeof value === "string" ? value : value.toString();
-    const length = text.length;
+class Serializer {
 
-    if(text.length + offset > BUFFER_SIZE)
-        throw new Error("Buffer size exceeeded");
+
+    constructor() {
+        this.BUILDER = Buffer.alloc(BUFFER_SIZE);
+        this.offset = 0;
+    }
+
+    serialize(json) {
+
+        this.offset = 0;
+        this.makeNode(json);
+        return this.BUILDER.toString('utf-8',0,this.offset);
+    }
+
+    /**
+     * Write value to buffer
+     * @param {*} value 
+     */
+    write(value) {
+        const text   = typeof value === "string" ? value : value.toString();
     
-    BUILDER.write(text,offset);
-    offset += text.length;
-}
-
-function makeNode(node) {
-
-    switch(typeof node) { 
-        case "object":
-            if("element" in node)
-                makeElement(node);
-            else if("comment" in node)
-                makeComment(node);
-            else if("processingInstruction" in node)
-                makeProcessingInstruction(node);
-            else if("dtdDeclaration" in node)
-                makeDTDDeclaration(node);
-            else
-                throw new Error(`Invalid node type (missing "element"?) in ${JSON.stringify(node)}`);    
-            break;
-        default:
-            write(node);
-            break;
+        if(text.length + this.offset > BUFFER_SIZE)
+            throw new Error("Buffer size exceeeded");
+        
+        this.BUILDER.write(text,this.offset);
+        this.offset += text.length;
     }
-}
 
-function makeElement({element,attributes,content}) {
-    const empty = content ? content.length == 0 : true;
+    makeNode(node) {
 
-    makeTag(element,attributes, empty ? 'EMPTY': 'START');
-    if(content) {
-        if(Array.isArray(content))
-            content.forEach( (child) => makeNode(child) );
-        else
-            throw new Error(`Content must be an array; cannot be: ${content}`);
-    }
-    if(!empty)
-        makeTag(element,null, 'END');
-}
-
-function makeComment({comment}) {
-    write(`<!--${comment}-->`);
-}
-
-function makeProcessingInstruction({processingInstruction,attributes}) {
-    write(`<?xml-${processingInstruction}`);
-    makeAttributes(attributes);
-    write(`?>`);
-}
-
-
-function makeTag(name,attributes,kind) {
-    write(`<${kind == 'END' ? '/' : ''}${name}`);
-    makeAttributes(attributes);
-    write(`${kind == 'EMPTY' ? '/' : ''}>`);
-}
-
-/**
- * 
- * @param {*} attributes 
- */
-function makeAttributes(attributes) {
-    if(attributes) {
-        if(typeof attributes === "object") {
-            for(const name in attributes) {
-                const value = attributes[name];
-                const sep   = bestDelimiter(value);
-                write(` ${name}=${sep}${value}${sep}`);
-            }
-        } else {
-            throw new Error(`Attributes must be an object, cannot be: ${attributes}`)
+        switch(typeof node) { 
+            case "object":
+                if("element" in node)
+                    this.makeElement(node);
+                else if("comment" in node)
+                    this.makeComment(node);
+                else if("processingInstruction" in node)
+                    this.makeProcessingInstruction(node);
+                else if("dtdDeclaration" in node)
+                    this.makeDTDDeclaration(node);
+                else
+                    throw new Error(`Invalid node type (missing "element"?) in ${JSON.stringify(node)}`);    
+                break;
+            default:
+                this.write(node);
+                break;
         }
     }
+
+    makeElement({element,attributes,content}) {
+        const empty = content ? content.length == 0 : true;
+    
+        this.makeTag(element,attributes, empty ? 'EMPTY': 'START');
+        if(content) {
+            if(Array.isArray(content))
+                content.forEach( (child) => this.makeNode(child) );
+            else
+                throw new Error(`Content must be an array; cannot be: ${content}`);
+        }
+        if(!empty)
+            this.makeTag(element,null, 'END');
+    }
+
+    makeComment({comment}) {
+        this.write(`<!--${comment}-->`);
+    }
+    
+    makeProcessingInstruction({processingInstruction,attributes}) {
+        this.write(`<?xml-${processingInstruction}`);
+        this.makeAttributes(attributes);
+        this.write(`?>`);
+    }
+
+    makeDTDDeclaration(dtdDeclaration) {
+        this.write(dtdDeclaration);
+    }
+
+    makeTag(name,attributes,kind) {
+        this.write(`<${kind == 'END' ? '/' : ''}${name}`);
+        this.makeAttributes(attributes);
+        this.write(`${kind == 'EMPTY' ? '/' : ''}>`);
+    }
+
+    /**
+     * 
+     * @param {*} attributes 
+     */
+    makeAttributes(attributes) {
+        if(attributes) {
+            if(typeof attributes === "object") {
+                for(const name in attributes) {
+                    const value = attributes[name];
+                    const sep   = this.bestDelimiter(value);
+                    this.write(` ${name}=${sep}${value}${sep}`);
+                }
+            } else {
+                throw new Error(`Attributes must be an object, cannot be: ${attributes}`)
+            }
+        }
+    }
+
+    /**
+     * Choose the best delimiter for an attribute, depending on its content 
+     * Should be improved
+     * 
+     * @param {*} value of attribute
+     * @returns either a double quote (default) or a single quote
+     */
+    bestDelimiter(value) {
+        const text = typeof value === "string" ? value : value.toString();
+        const pos  = text.indexOf('\"');
+
+        return (pos > -1 && text[pos-1] != '\\') ? '\'' : '\"' ;
+    }    
 }
 
-/**
- * Choose the best delimiter for an attribute, depending on its content 
- * Should be improved
- * 
- * @param {*} value of attribute
- * @returns either a double quote (default) or a single quote
- */
-function bestDelimiter(value) {
-    const text = typeof value === "string" ? value : value.toString();
-    const pos  = text.indexOf('\"');
 
-    return (pos > -1 && text[pos-1] != '\\') ? '\'' : '\"' ;
+
+
+// ----------
+
+class Parser {
+
+    /**
+     * 
+     * @param {string} text to parse 
+     */
+    constructor(text) {
+        this.text = text;
+    }
+
+    parse() {
+        this.current = 0;
+        this.lineCount = 0;
+        const content = this.parseContent();
+
+        switch(content.length) {
+            case 0:
+                this.error('empty document');
+            case 1:
+                return content[0];
+            default: // document with declaration and/or preamble
+                this.error('not implemented yet');
+        }
+    }
+
+    parseContent(name) {
+        const content = [];
+        let token;
+
+        while(token = this.nextToken()) {
+            switch(token.kind) {
+                case 'STRING':
+                    content.push(token.text);
+                    break;
+                case 'EMPTY':
+                    const node = { element: token.name };
+                    if(token.attributes)
+                        node.attributes = token.attributes;
+                    content.push(node); 
+                    break;
+                case 'START':
+                    const parsedContent = this.parseContent(token.name);
+                    const element = { element: token.name };
+
+                    if(token.attributes)
+                        element.attributes = token.attributes
+                    if(parsedContent.length > 0)
+                        element.content = parsedContent;
+
+                    content.push(element);
+
+                    break;
+                case 'END':
+                    if(token.name === name)
+                        return content;
+                    else 
+                        this.error(`invalid close tag </${token.name}>, expected </${name}>`);
+                case 'EXCLAMATION':
+                    const found = token.text.match(/<!--(.*)-->/s);
+                    
+                    if(found) 
+                        content.push({ comment: found[1] });
+                    else
+                        content.push({ dtdDeclaration: token.text });
+                    break;
+
+                case 'QUESTION':
+
+                    if(token.name === 'xml') {
+                        if(this.current - token.text.length > 0)
+                            this.error(`XML declaration must start the document`);
+                        else
+                            content.push({xmlDeclaration: '', attributes: token.attributes });        
+                    } else if (token.name.statsWith('xml-'))
+                        content.push({ 
+                            processingInstruction: token.name,
+                            attributes: token.attributes
+                        });
+                    else
+                        this.error(`invalid processing instruction ${token.text}`);
+                    break;
+                default:
+                    this.error(`non implemented token ${token.kind}`);
+            }
+        }
+        return content;
+    }
+
+    /**
+     * 
+     * @returns next token in text, or null if none
+     */
+    nextToken() {
+        if(this.current == this.text.length)
+            return null;
+
+        const pos = this.text.indexOf('<',this.current);
+        
+        if(pos == this.current) {
+            const end = this.text.indexOf('>',this.current);
+
+            if(end == -1)
+                this.error('expected >');
+            
+            const text = this.consume(this.current,end+1);
+            return this.makeToken(text);
+
+        } else {
+            const end = pos > 0 ? pos : undefined ;
+            const text = this.consume(this.current,end);
+
+            return { kind: 'STRING', text };
+        } 
+    }
+
+    /**
+     * The name of the tag with given text
+     * @param {string} text 
+     * @returns name of tag
+     */
+    makeToken(text) {
+        const found = text.match(/<([\?\/\!]?)([^\s\/\>]+)([^\/>]*)(\/?)>/m);
+
+        if(found) {
+            const kind  = this.makeKind(found[1],found[4]);
+            const name  = found[2];
+            const token = { kind, name, text };
+            
+            if(found[3]) {
+                token.attributes = {};
+
+                for(const [_all,name,_del,value] of found[3].matchAll(
+                    /\s*([\w:_]*)\s*=\s*(?<d>"|')(.+?)(\k<d>)/gs)) 
+                        token.attributes[name] = value; 
+            }
+            return token;
+        } else
+            return null;
+    }
+    
+    /**
+     * Decide on kind of tag based on left and right 
+     * special characters:  / ? !
+     * @param {string} left 
+     * @param {string} right 
+     * @returns 
+     */
+    makeKind(left,right) {
+        const LEFT = {
+            '/': 'END',
+            '?': 'QUESTION',
+            '!': 'EXCLAMATION'
+        }
+
+        const RIGHT = {
+            '/': 'EMPTY'
+        };
+    
+        if(left != '') {
+            if(right != '')
+                this.error(`invalid tag: ${text}`);
+            else
+                return LEFT[left];
+        } else {
+            if(right != '')
+                return RIGHT[right];
+            else
+                return 'START';
+        }
+    }
+
+    /**
+     * Number of newlines in text
+     * @param {string} text with newlines 
+     * @returns count 
+     */
+    consume(start,end) {
+        const consumed = this.text.substring(start,end);
+
+        this.lineCount += consumed.match(/\n/g)?.length ?? 0;
+        this.current = end;
+
+        return consumed;
+    }
+
+    /**
+     * Throws an error with with message 
+     * and line number were it occured
+     *  
+     * @param {string} message 
+     */
+    error(message) {
+        throw new Error(`Error in line ${this.lineCount}: ${message}`);
+    }
+
 }
 
+module.exports = { 
 
+    parse: function(dtd) {
+        const parser = new Parser(dtd);
 
-// parse: parse,
+        return parser.parse();
+    }, 
 
-module.exports = {
+    serialize: function(json) {
+        const serializer = new Serializer();
 
-    serialize: serialize
+        return serializer.serialize(json);
+    }
 };
