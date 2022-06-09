928e2bee5c47289e6892ddcdfd8aa4926af30452
José Paulo Leal <zp@dcc.fc.up.pt>
2022-06-09 20:19:27 +0100

    Updated dubugging tests

--- test/debug.js
+++ test/debug.js
@@ -1,20 +1,55 @@
+
 const assert = require('assert');
-const { serialize, parse } = require('../xmlinjson.js');
-const { jsonTypeOf, validate, instances, nonInstances }  = require('../generator.js');
+const { getJsonSchema, validate, instances, nonInstances } = require('../dtd');
 
-describe('Debug', () => {
 
-    const schema = {
-        type: "array",
-        prefixItems: [ 
-            { type: "boolean", minOccurs: 0},
-            { type: "number" }
-        ],
-    };
-    const value = [ 1 ];
+describe('DTD and documents',() => {
 
-    it(`check validate ${JSON.stringify(value)}`, (done) => {
-        assert( validate(schema,value) );
-        done();
-    });
+    const examples = [
+        {
+            description: 'Simple DTD with just top empty element',
+            dtd: `<!DOCTYPE top [
+                <!ELEMENT top EMPTY>
+            ]>`,
+            expected:  {
+                definitions: {
+                    'top': {
+                        $id: 'top',
+                        type: 'object',
+                        properties: { element: { const: 'top'} }
+                    }
+                },
+                $ref: 'top'
+            },
+            instances: [ '<top/>' ],
+            nonInstances: [ 
+                //'<root/>', 
+                //'<top>hello</top>', 
+                '<top a="1"/>' 
+            ]
+        }
+    ];
+
+    for(const example of examples)
+        describe(example.description, () => {
+            /*
+            it('Check JSON Schema', (done) => { 
+                similarTo(getJsonSchema(example.dtd),example.expected); 
+                done();
+            });
+
+            for(const instance of example.instances)
+                it(`validate instance ${instance}`, (done) => { 
+                    assert(validate(example.dtd,instance)); 
+                    done(); 
+                });
+        */
+            for(const nonInstance of example.nonInstances)
+                it(`invalidate non-instace ${nonInstance}`, (done) => { 
+                    assert(! validate(example.dtd,nonInstance)); 
+                    done(); 
+                });
+
+        });
 });
+
